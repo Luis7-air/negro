3501fb7ec8267265b631d424abe7628744013d9f
Luis7-air <vr564226@gmail.com>
2025-08-04 20:22:43 +0000

    negra

---
+++ main.js
@@ -0,0 +1,23 @@
+let negro1 = 1 
+negro = negro + ",sos muy negro"
+
+negro = "washa"
+
+let verdadero = true
+let falso = false 
+let booleano = divididr 
+
+negro = -12.424
+negro = negro + 1
+negro += 1
+negro++ 
+negro = 20 + 5 - 10 + 30
+let blanco = 1000 * 500
+let blanconegro = 50 / 25
+let booleano = dividir == 12.5
+console.log(booleano)
+let mayor = 1 > 0
+let menor = 1 < 0
+let mayorigual = 1 >= 0
+let menorigual = 1 <= 0 
+let los2sonverdaderos = true && false
