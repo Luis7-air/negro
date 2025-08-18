d385a72d99c61704d63ba2dad76ecaa95510388c
Luis7-air <vr564226@gmail.com>
2025-08-18 20:00:58 +0000

    negri

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let negro1 = 1 
+let negro =1
 negro = negro + ",sos muy negro"
 
 negro = "washa"
 
 let verdadero = true
 let falso = false 
-let booleano = divididr 
+let booleano = 1/2 
 
 negro = -12.424
 negro = negro + 1
@@ -14,7 +14,7 @@ negro++
 negro = 20 + 5 - 10 + 30
 let blanco = 1000 * 500
 let blanconegro = 50 / 25
-let booleano = dividir == 12.5
+booleano = booleano == 12.5
 console.log(booleano)
 let mayor = 1 > 0
 let menor = 1 < 0
